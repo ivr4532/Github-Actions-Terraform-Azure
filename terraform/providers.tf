2083b2a290b9f2bcb98abb7a05672daa821b7fee
ivr4532 <56123816+ivr4532@users.noreply.github.com>
2025-09-13 18:43:54 -0500

    provide formatted

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -8,6 +8,6 @@ terraform {
 }
 provider "azurerm" {
   subscription_id = "0183dc3c-a203-4302-85c9-038087a640d0"
-  tenant_id = ca13c763-d3f0-4dfc-8989-b571d8afff7c
-features {}
+  tenant_id       = ca13c763-d3f0-4dfc-8989-b571d8afff7c
+  features {}
 }
