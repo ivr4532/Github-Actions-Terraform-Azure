3d0db6b2c56f3b1e01113296e511cbd66592a40b
ivr4532 <56123816+ivr4532@users.noreply.github.com>
2025-09-13 18:37:44 -0500

    Update providers.tf

--- terraform/providers.tf
+++ terraform/providers.tf
@@ -2,12 +2,12 @@ terraform {
   required_providers {
     azurerm = {
       source  = "hashicorp/azurerm"
-      version = "=2.46.0"
+      version = ">=2.46"
     }
   }
 }
 provider "azurerm" {
   subscription_id = "0183dc3c-a203-4302-85c9-038087a640d0"
   tenant_id = ca13c763-d3f0-4dfc-8989-b571d8afff7c
-  features {}
+features {}
 }
